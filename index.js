d92f991973e4841660b37004e5088e56c71404a6
Anika <anikaafrin520@gmail.com>
2024-02-26 22:51:49 +0600

    ok

---
+++ index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import App from './App'; // adjust the import path if necessary
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
